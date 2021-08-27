62c170fd48d1928440c3390c885902d62c76e415
Andreas <andreas.vogt89@bluewin.ch>
2021-08-27 21:15:57 +0200

    startet with bacnet objects types

--- src/logger.js
+++ src/logger.js
@@ -1,44 +1,44 @@
-const Transport = require('winston-transport');
-const { createLogger, transports, format } = require('winston');
-const window = require('electron').BrowserWindow;
+const Transport = require("winston-transport");
+const { createLogger, transports, format } = require("winston");
+const window = require("electron").BrowserWindow;
 
 class ConsoleFrontend extends Transport {
   constructor(opts) {
     super(opts);
   }
 
-  log(info,callback) {
+  log(info, callback) {
     setImmediate(() => {
-      this.emit('logged', info);
+      this.emit("logged", info);
     });
     let win = window.getFocusedWindow();
-    win.webContents.send('CONSOLE_MSG', {
-      level:info.level,
-      message:info.message,
-      timestamp:info.timestamp,
+    win.webContents.send("CONSOLE_MSG", {
+      level: info.level,
+      message: info.message,
+      timestamp: info.timestamp,
     });
-    if(callback){
+    if (callback) {
       callback();
     }
   }
 }
 
 const logger = createLogger({
-    format: format.combine(
-        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
-        format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
-    ),
-    transports: [
-        new transports.File({
-            filename: './logs/log.log',
-            json: false,
-            maxsize: 5242880,
-            maxFiles: 1,
-            level: 'warn',
-        }),
-        new transports.Console(),
-        new ConsoleFrontend(),
-    ]
+  format: format.combine(
+    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss:ms" }),
+    format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
+  ),
+  level: "debug",
+  transports: [
+    new transports.File({
+      filename: "./logs/log.log",
+      json: false,
+      maxsize: 5242880,
+      maxFiles: 1,
+    }),
+    new transports.Console(),
+    new ConsoleFrontend(),
+  ],
 });
 
 module.exports = logger;
