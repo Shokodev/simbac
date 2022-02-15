306c972caac6f7a99b61ea2fe1a4370bd0c9017d
andreasvogt89 <30302212+andreasvogt89@users.noreply.github.com>
2022-02-15 21:20:27 +0100

    testdevice wirte/read commands

--- testdevice/index.js
+++ testdevice/index.js
@@ -1,88 +1,146 @@
-import bacnet from 'bacstack';
+import bacnet from "bacstack";
+import os from "os";
 
-const deviceId = process.env.DEVICEID || 1234;
+const bacId = process.env.BACID || 123456;
 const port = process.env.BACPORT || 47808;
 const CMD = {
-    info:(msg)=>{console.log(`[INFO]  : ${msg}`)},
-    error:(msg)=>{console.log(`[ERROR]: ${msg}`)},
+  info: (msg) => {
+    console.log(`[INFO]  : ${msg}`);
+  },
+  error: (msg) => {
+    console.log(`[ERROR] : ${msg}`);
+  },
 };
 
+const getInterface = () => {
+  const itfc = os.networkInterfaces();
+  return Object.keys(itfc).reduce((acc, val) => {
+    itfc[val].forEach((addr) => {
+      if (addr.family === "IPv4") {
+        acc = addr.address;
+      }
+    });
+    return acc;
+  }, "");
+};
 
-if(!process.env.INTERFACE){
-     CMD.error('You need to specify the network interface with ENV, as example INTERFACE=192.168.0.50');
-     process.exit(0);
-} 
+if (!process.env.INTERFACE) {
+  CMD.info(
+    `No env with interface address provided => serach local interfaces. . .`
+  );
+  process.env.INTERFACE = getInterface();
+}
 
 const bacstack = new bacnet({
-    port: port,
-    interface: process.env.INTERFACE,
+  port: port,
+  interface: process.env.INTERFACE,
 });
 
 bacstack.on("whoIs", (data) => {
-    CMD.info(`Who is request received from ${data.deviceId}`);
-    if (data.lowLimit && data.lowLimit > deviceId) return;
-    if (data.highLimit && data.highLimit < deviceId) return;
-    bacstack.iAmResponse(
-        deviceId,
-        bacnet.enum.Segmentation.SEGMENTATION_BOTH,
-        99
-        ); 
-    });
-
-
-bacstack.on('error', (err) => {
-    CMD.error(`Stack failed with: ${err}`);
-    bacstack.close();
-    process.exit(2);
+  CMD.info(`Who is request received from ${data.deviceId}`);
+  if (data.lowLimit && data.lowLimit > bacId) return;
+  if (data.highLimit && data.highLimit < bacId) return;
+  bacstack.iAmResponse(bacId, bacnet.enum.Segmentation.SEGMENTATION_BOTH, 99);
 });
 
-CMD.info(`Test device started with id: [${deviceId}] on: [${process.env.INTERFACE}:${port}]`);
+bacstack.on("error", (err) => {
+  CMD.error(`Stack failed with: ${err}`);
+  bacstack.close();
+  process.exit(2);
+});
+
+CMD.info(
+  `Test device started with id: [${bacId}] on: [${process.env.INTERFACE}:${port}]`
+);
 process.stdin.setEncoding("utf8");
 process.stdin.on("data", async (data) => {
-    let input = data.trim();
-    if(input === 'exit') process.exit(0);
-    if(input.startsWith('whois')) {
-        let splits = input.split(' ');
-        CMD.info("Sending whoIs . . .");
-        if(splits[1]){
-            bacstack.whoIs({address:splits[1]});    
-        } else {
-            bacstack.whoIs();
-        }
+  let input = data.trim();
+  if (input === "exit") process.exit(0);
+  if (input.startsWith("whois")) {
+    let splits = input.split(" ");
+    CMD.info("Sending whoIs . . .");
+    if (splits[1]) {
+      bacstack.whoIs({ address: splits[1] });
+    } else {
+      bacstack.whoIs();
     }
-    if(input.startsWith('read')) {
-        let splits = input.split(' ');
-        if(splits.length !== 4){
-            CMD.error("Wrong Parameter, use example: read 192.168.0.50 0:3 85");
-        } else {
-            await readprop(splits);
-        }
+  }
+  if (input.startsWith("read")) {
+    let splits = input.split(" ");
+    if (splits.length !== 4) {
+      CMD.error("Wrong Parameter, use example: read 192.168.0.50 0:3 85");
+    } else {
+      await progressRequest(readprop, splits);
     }
+  }
+  if (input.startsWith("write")) {
+    let splits = input.split(" ");
+    if (splits.length !== 6) {
+      CMD.error(
+        "Wrong Parameter, use example: write 192.168.0.50 0:3 85 4 1.8"
+      );
+    } else {
+      await progressRequest(writeprop, splits);
+    }
+  }
 });
 
-const readprop = async (s) => {
-    try {
-        let r = await new Promise((re,rj)=>{
-        let prog = " . "
-        let loop = setInterval(async()=>{  
-            if(prog === ". ") {
-                prog = ". . ";
-            } else if(prog === ". . "){ 
-                prog = ". . . ";
-            } else prog = ". ";
-            process.stdout.clearLine()
-            process.stdout.cursorTo(0)
-            process.stdout.write(`Reading${prog}`);
-        });     
-        bacstack.readProperty(s[1], {type: s[2].split(':')[0], instance: s[2].split(':')[1]}, s[3], (err, data) => {
-            clearInterval(loop);
-            console.log(``);
-            if(err){rj(err)}else{
-            re(data.values[0].value);}    
-        });
-        });
-        CMD.info(r);
-    } catch(err){
-        CMD.error(err);
-    }   
-}
+const readprop = async (s, cb) => {
+  try {
+    bacstack.readProperty(
+      s[1],
+      { type: s[2].split(":")[0], instance: s[2].split(":")[1] },
+      s[3],
+      (err, data) => {
+        if (err) {
+          cb(`ERR::${err}`);
+        } else cb(data.values[0].value);
+      }
+    );
+  } catch (err) {
+    cb(`ERR::${err}`);
+  }
+};
+const writeprop = async (s, cb) => {
+  try {
+    bacstack.writeProperty(
+      s[1],
+      { type: s[2].split(":")[0], instance: s[2].split(":")[1] },
+      s[3],
+      [{ type: parseInt(s[4]), value: s[5] }],
+      (err, data) => {
+        if (err) {
+          cb(`ERR::${err}`);
+        } else cb(data.values[0].value);
+      }
+    );
+  } catch (err) {
+    cb(`ERR::${err}`);
+  }
+};
+const progressRequest = async (req, splits) => {
+  try {
+    let r = await new Promise((re, rj) => {
+      let loop = setInterval(async () => {
+        for(let i = 0; i < 50; i++){
+            process.stdout.clearLine();
+            process.stdout.cursorTo(0);
+            let prog = "=".repeat(i);
+            let spare = " ".repeat(49 - i); 
+            process.stdout.write(`Progress: [${prog}${spare}]`);
+        }  
+      });
+      req(splits, (result) => {
+        clearInterval(loop);
+        process.stdout.clearLine();
+        process.stdout.cursorTo(0);
+        if (result.toString().startsWith("ERR::"))
+          rj(result.split("ERR::")[1].replace("Error:", ""));
+        re(result);
+      });
+    });
+    CMD.info(r);
+  } catch (err) {
+    CMD.error(err);
+  }
+};
--- testdevice/readme.md
+++ testdevice/readme.md
@@ -3,14 +3,17 @@ This device can be used to test against the simbac simulator.
 
 > You can not run this code on the same machine where simbac or any other bacnet stack is running!
 
-Easiest way to use this code is to run it in a docker container which runs in host mode. You can build the docker image by yourself with the provided Dockerfile or just use the image from docker hub with:
+Easiest way to use this code is to run it in a docker container. You can build the docker image by yourself with the provided Dockerfile or just use the image from docker hub with:
 
 ```bash
-docker run --network="host" --env INTERFACE="<your IP>" dinifarb/simbac_test
+docker run -d dinifarb/simbac_testdevice
 ```
 
->Don't forget to change the INTERFACE variable to your machines IP which has to be in the same LAN as the simbac instance
+Following settings are changable with env:
 
+- INTERFACE=ipaddress (Default last ipv found by local interfaces)
+- PORT=47808 (Bacnet Port, default 47808)
+- BACID=1234 (Bacnet ID, default 123456)
 
 As soon the container is started you can ssh in it and run `node index.js` in the app root dir. After the app has started you can use the below commands:
 
@@ -19,3 +22,18 @@ As soon the container is started you can ssh in it and run `node index.js` in th
 |-|-|-|-|
 |`whois`|{addr} `optional`|`whois` or `whois 192.168.0.44`|sends a whoIs to broadcast addr
 |`read`|{addr} {objectidentifier} {propertyidentifier}|`read 192.168.0.44 0:1 85`|sends a read property request|
+|`write`|{addr} {objectidentifier} {propertyidentifier} {property value type} {value}|`read 192.168.0.44 0:1 85 4 1.7`|sends a write property request|
+
+some property value types:
+  - BOOLEAN = 1
+  - UNSIGNED_INTEGER = 2
+  - SIGNED_INTEGER = 3
+  - REAL = 4
+  - DOUBLE = 5
+  - OCTET_STRING = 6
+  - CHARACTER_STRING = 7
+  - BIT_STRING = 8
+  - ENUMERATED = 9
+  - DATE = 10
+  - TIME = 11
+  - OBJECTIDENTIFIER = 12
